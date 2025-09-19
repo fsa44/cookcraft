00db13b2284f4845d82cb53305ab9892156afd13
fsa44 <fatmasarah.abdiraham@strathmore.edu>
2025-09-19 21:49:52 +0300

    Update splash screen

--- Cookcraft/CookcraftApp.swift
+++ Cookcraft/CookcraftApp.swift
@@ -4,19 +4,49 @@
 //
 //  Created by Fatmasarah Abdikadir on 04/06/2025.
 //
+//
+//import SwiftUI
+//import Firebase
+//
+//@main
+//struct CookcraftApp: App {
+//    init(){
+//        FirebaseApp.configure()
+//    }
+//    
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
+
 
 import SwiftUI
 import Firebase
 
 @main
 struct CookcraftApp: App {
-    init(){
-        FirebaseApp.configure()
+    @State private var showingSplash = true
+
+    init() {
+        FirebaseApp.configure() // Firebase initialization
     }
-    
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if showingSplash {
+                // Show the splash screen first
+                SplashView {
+                    withAnimation {
+                        showingSplash = false
+                    }
+                }
+                .transition(.opacity) // Optional fade effect for splash screen
+            } else {
+                // Once splash is done, show the main content
+                ContentView()
+            }
         }
     }
 }
